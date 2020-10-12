b2605b01b6f772d93803723ae209d9a9719111bf
yyoel <yoelyulius@gmail.com>
2020-10-12 13:08:29 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,119 @@
+
+import json
+from typing import Hashable
+
+import pika
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+
+from model.models import Base, HC_EmployeeCareer
+
+
+def main():
+    # Create connection to MSSQL DWHDB
+    engine = sqlalchemy.create_engine(
+        'mssql://dwhdb/UC_DEV?driver=SQL Server')      
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    # RabbitMQ Connection 
+    credentials = pika.PlainCredentials('guest','guest')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='172.16.4.14', credentials=credentials)
+    )
+    
+    channel = connection.channel()
+
+
+    def callback (ch, method, properties, body):
+
+        # Turn JSON into Python Dictionary
+        data = json.loads(body)
+           
+        # Save Data
+        save_to_db(data)
+
+    def save_to_db(data):
+        # print(data)
+        exist = session.query(HC_EmployeeCareer.emp_no).filter_by(emp_no=data['emp_no']).scalar()
+        print(exist)
+        if exist == None:
+            employee = HC_EmployeeCareer(
+            data['emp_no'],
+            data['emp_name'],
+            data['emp_status'],
+            data['dt_join'],
+            data['dt_rehire_mitra'],
+            data['dt_resign'],
+            data['dt_birthday'],
+            data['company_id'],
+            data['company_name'],
+            data['directorate_code'],
+            data['directorate_name'],
+            data['division_code'],
+            data['division_name'],
+            data['department_code'],
+            data['department_name'],
+            data['subdepartment_code'],
+            data['subdepartment_name'],
+            data['section_code'],
+            data['section_name'],
+            data['job_title_code'],
+            data['job_title_name'],
+            data['position_code'],
+            data['position_name'],
+            data['job_grade'],
+            data['worklocation_code'],
+            data['worklocation_name'],
+            data['psikotest'],
+            data['corporate_email'],
+            data['mobile_phone_no'],
+            data['superior_emp_no'],
+            data['superior_emp_name'],
+            data['gender'],
+            data['education'],
+            data['resign_reason'],
+            data['last_career_type'],
+            data['directorate_catalog_code'],
+            data['directorate_catalog_name'],
+            data['division_catalog_code'],
+            data['division_catalog_name'],
+            data['department_catalog_code'],
+            data['department_catalog_name'],
+            data['unit_catalog_code'],
+            data['unit_catalog_name'],
+            data['satuan_kerja_code'],
+            data['satuan_kerja_name'],
+            data['measure_id'],
+            data['measure_name'],
+            data['cost_allocation_code'],
+            data['cost_allocation_name'],
+            data['region_allocation_code'],
+            data['region_allocation_name'],
+        )
+
+            session.add(employee)
+            session.commit()
+            print('Data inserted\n', data)
+        else:
+            print('This data', data['emp_no'],'already exist')
+
+    # Create Table 
+    if HC_EmployeeCareer.__tablename__ not in engine.table_names():
+        Base.metadata.tables['UC58_HC_EmployeeCareer'].create(bind=engine)
+        print('Table created.')
+
+    # Consume Message
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='q.human-capital.master-employee-data-analytics.work',
+                          auto_ack=True,
+                          on_message_callback=callback)
+
+    print('[x] Waiting for Message. To exit press CTRL+C')
+    channel.start_consuming()
+    channel.basic_qos()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ model/models.py
@@ -0,0 +1,121 @@
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class HC_EmployeeCareer(Base):
+    __tablename__ = 'UC58_HC_EmployeeCareer'
+
+    id = Column(Integer, primary_key=True)
+    emp_no = Column(String)
+    emp_name = Column(String)
+    emp_status = Column(String)
+    dt_join = Column(Date)
+    dt_rehire_mitra = Column(Date)
+    dt_resign = Column(Date)
+    dt_birthday = Column(Date)
+    company_id = Column(String)
+    company_name = Column(String)
+    directorate_code = Column(String)
+    directorate_name = Column(String)
+    division_code = Column(String)
+    division_name = Column(String)
+    department_code = Column(String)
+    department_name = Column(String)
+    subdepartment_code = Column(String)
+    subdepartment_name = Column(String)
+    section_code = Column(String)
+    section_name = Column(String)
+    job_title_code = Column(String)
+    job_title_name = Column(String)
+    position_code = Column(String)
+    position_name = Column(String)
+    job_grade = Column(String)
+    worklocation_code = Column(String)
+    worklocation_name = Column(String)
+    psikotest = Column(String)
+    corporate_email = Column(String)
+    mobile_phone_no = Column(String)
+    superior_emp_no = Column(String)
+    superior_emp_name = Column(String)
+    gender = Column(String)
+    education = Column(String)
+    resign_reason = Column(String)
+    last_career_type = Column(String)
+    directorate_catalog_code = Column(String)
+    directorate_catalog_name = Column(String)
+    division_catalog_code = Column(String)
+    division_catalog_name = Column(String)
+    department_catalog_code = Column(String)
+    department_catalog_name = Column(String)
+    unit_catalog_code = Column(String)
+    unit_catalog_name = Column(String)
+    satuan_kerja_code = Column(String)
+    satuan_kerja_name = Column(String)
+    measure_id = Column(String)
+    measure_name = Column(String)
+    cost_allocation_code = Column(String)
+    cost_allocation_name = Column(String)
+    region_allocation_code = Column(String)
+    region_allocation_name = Column(String)
+
+    def __init__(self, emp_no, emp_name, emp_status, dt_join, dt_rehire_mitra,
+                 dt_resign, dt_birthday, company_id, company_name, directorate_code, directorate_name,
+                 division_code, division_name, department_code, department_name, subdepartment_code, subdepartment_name,
+                 section_code, section_name, job_title_code, job_title_name, position_code, position_name, job_grade, worklocation_code, worklocation_name,
+                 psikotest, corporate_email, mobile_phone_no, superior_emp_no, superior_emp_name, gender, education, resign_reason, last_career_type,
+                 directorate_catalog_code, directorate_catalog_name, division_catalog_code, division_catalog_name, department_catalog_code, department_catalog_name,
+                 unit_catalog_code, unit_catalog_name, satuan_kerja_code, satuan_kerja_name, measure_id, measure_name, cost_allocation_code, cost_allocation_name,
+                 region_allocation_code, region_allocation_name):
+
+                 self.emp_no = emp_no
+                 self.emp_name = emp_name
+                 self.emp_status = emp_status
+                 self.dt_join = dt_join
+                 self.dt_rehire_mitra = dt_rehire_mitra
+                 self.dt_resign = dt_resign
+                 self.dt_birthday = dt_birthday
+                 self.company_id = company_id
+                 self.company_name = company_name
+                 self.directorate_code = directorate_code
+                 self.directorate_name = directorate_name
+                 self.division_code = division_code
+                 self.division_name = division_name
+                 self.department_code = department_code
+                 self.department_name = department_name
+                 self.subdepartment_code = subdepartment_code
+                 self.subdepartment_name = subdepartment_name
+                 self.section_code = section_code
+                 self.section_name = section_name
+                 self.job_title_code = job_title_code
+                 self.job_title_name = job_title_name
+                 self.position_code = position_code
+                 self.position_name = position_name
+                 self.job_grade = job_grade
+                 self. worklocation_code = worklocation_code
+                 self.worklocation_name = worklocation_name
+                 self.psikotest = psikotest
+                 self.corporate_email = corporate_email
+                 self.mobile_phone_no = mobile_phone_no
+                 self.superior_emp_no = superior_emp_no
+                 self.superior_emp_name = superior_emp_name
+                 self.gender = gender
+                 self.education = education
+                 self.resign_reason = resign_reason
+                 self.last_career_type = last_career_type
+                 self.directorate_catalog_code =directorate_catalog_code
+                 self.directorate_catalog_name = directorate_catalog_name
+                 self.division_catalog_code = division_catalog_code
+                 self.division_catalog_name = division_catalog_name
+                 self.department_catalog_code = department_catalog_code
+                 self.department_catalog_name = department_catalog_name
+                 self.unit_catalog_code = unit_catalog_code
+                 self.unit_catalog_name = unit_catalog_name
+                 self.satuan_kerja_code = satuan_kerja_code
+                 self.satuan_kerja_name = satuan_kerja_name
+                 self.measure_id = measure_id
+                 self.measure_name = measure_name
+                 self.cost_allocation_code = cost_allocation_code
+                 self.cost_allocation_name = cost_allocation_name
+                 self.region_allocation_code = region_allocation_code
+                 self.region_allocation_name = region_allocation_name
